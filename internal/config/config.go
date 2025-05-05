e8f10daf70d98eb71e93301f38f42e389cc50a78
Reshord <daribayev1982@bk.ru>
2025-05-05 15:18:03 +0500

    Улучшена обработка ошибок при загрузке конфигурации; исправлены ключи окружения для сервера и базы данных.

--- internal/config/config.go
+++ internal/config/config.go
@@ -56,20 +56,29 @@ func LoadConfig(path string) (*Config, error) {
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("ошибка чтения конфигурационного файла: %w", err)
 	}
 
 	var config Config
-	config.Server.Port = viper.GetString("SERVER_PORT")
-	config.Server.ReadTimeout = viper.GetDuration("SERVER_READ_TIMEOUT")
-	config.Server.WriteTimeout = viper.GetDuration("SERVER_WRITE_TIMEOUT")
 
-	config.Database.Host = viper.GetString("DB_HOST")
-	config.Database.Port = viper.GetString("DB_PORT")
-	config.Database.User = viper.GetString("DB_USER")
-	config.Database.Password = viper.GetString("DB_PASSWORD")
-	config.Database.DBName = viper.GetString("DB_NAME")
-	config.Database.SSLMode = viper.GetString("DB_SSLMODE")
+	config.Server.Port = viper.GetString("server.port")
+	config.Server.ReadTimeout = viper.GetDuration("server.read_timeout")
+	config.Server.WriteTimeout = viper.GetDuration("server.write_timeout")
+
+	config.Database.Host = viper.GetString("database.host")
+	config.Database.Port = viper.GetString("database.port")
+	config.Database.User = viper.GetString("database.user")
+	config.Database.Password = viper.GetString("database.password")
+	config.Database.DBName = viper.GetString("database.dbname")
+	config.Database.SSLMode = viper.GetString("database.sslmode")
+
+	config.APILogin = viper.GetString("iiko.api_login")
+	config.TokenCacheKey = viper.GetString("iiko.token_cache_key")
+	config.TokenTimeout = viper.GetDuration("iiko.token_timeout")
+	config.APITimeout = viper.GetDuration("iiko.api_timeout")
+	config.WaiterAPIURL = viper.GetString("iiko.waiter_api_url")
+	config.WaiterAPIKey = viper.GetString("iiko.waiter_api_key")
+	config.DefaultWaiterID = viper.GetString("iiko.default_waiter_id")
 
 	return &config, nil
 }
